a1ac05e0eb92344bce13909e6f6da919db051926
vj0r_vj0r <vj0r@info-linux.ru>
2020-07-04 14:28:25 +0300

    Perl script

--- count_ip.pl
+++ count_ip.pl
@@ -4,25 +4,17 @@ open(F1,"./file_ip");
 $line = join('', <F1>);
 @mass = $line=~m/((?:[0-9]{1,3}\.){3}(?:[0-9]{1,3}))/g;
 map { $hash{$_} = undef } @mass;
-print "\nArray count : ".scalar @mass."\n";
-
+print "\nnumber of elements in the array: ".scalar @mass."\n";
 %hash_result;
-
 while( ($k,$v) = each %hash ){
-
 	$n = 0;
-
 	for $i (@mass) {
 		if ($i eq $k) {
 			$n++;
 		}
 	}
 	$hash_result{$k} = $n;
-	
 }
-
-print "Результат\n";
 while( ($k,$v) = each %hash_result ){
-
-	print "ip : ". $k ." count ". $v ."\n";
+	print "ip: ". $k ." count: ". $v ."\n";
 }
